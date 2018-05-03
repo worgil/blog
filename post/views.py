9e32383ae031497c3e36329237423018b9e46b6f
worgil <Ad2tr5447>
2018-05-03 23:55:39 +0300

    blog çalışması pyhton django

--- post/views.py
+++ post/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def post_index(request):
+    return HttpResponse('<b> Burası Anasayfa</b>')
+
+
+def post_detail(request):
+    return HttpResponse('<b> Burası Detay</b>')
+
+
+def post_create(request):
+    return HttpResponse('<b> Burası Create</b>')
+
+
+def post_update(request):
+    return HttpResponse('<b> Burası Update</b>')
+
+
+def post_delete(request):
+    return HttpResponse('<b> Burası Delete</b>')
